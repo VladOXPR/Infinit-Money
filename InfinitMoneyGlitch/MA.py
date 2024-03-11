e91c944b42418afda5df88e4547c7fa23b0df9df
vladvalchkov <vladvalchkov541@gmail.com>
2024-03-10 22:15:44 -0500

    Organize readablility

--- InfinitMoneyGlitch/MA.py
+++ InfinitMoneyGlitch/MA.py
@@ -1,28 +1,29 @@
-import matplotlib.pyplot as plt
 import yfinance as yf
+import matplotlib.pyplot as plt
 
-plt.style.use("dark_background")
 
+# Data setup
 ma_1 = 12
 ma_2 = 26
 
 symbol = 'BTC'
 data = yf.download(symbol, period='1y', interval='1d')
-
 data[f'MA_{ma_1}'] = data['Adj Close'].rolling(window=ma_1).mean()
 data[f'MA_{ma_2}'] = data['Adj Close'].rolling(window=ma_2).mean()
-
 data = data.iloc[ma_2:]
 
+
+# Variables
 buy_signals = []
 sell_signals = []
+
 trigger = 1
 balance = 10000
 
 
+# Algorithm
 for x in range(len(data)):
     share_price = data['Adj Close'].iloc[x]
-
     if data[f'MA_{ma_1}'].iloc[x] > data[f'MA_{ma_2}'].iloc[x] and trigger != -1:
         buy_signals.append(data['Adj Close'].iloc[x])
         sell_signals.append(float('nan'))
@@ -30,7 +31,6 @@ for x in range(len(data)):
         print('Bought', share_price)
         balance = balance - share_price
         print('Balance', balance)
-
     elif data[f'MA_{ma_1}'].iloc[x] < data[f'MA_{ma_2}'].iloc[x] and trigger != 1:
         buy_signals.append(float('nan'))
         sell_signals.append(data['Adj Close'].iloc[x])
@@ -38,12 +38,13 @@ for x in range(len(data)):
         print('Sold ', share_price)
         balance = balance + share_price
         print('Balance', balance)
-
     else:
         buy_signals.append(float('nan'))
         sell_signals.append(float('nan'))
 
 
+# Plotting
+plt.style.use("dark_background")
 data['Buy Signals'] = buy_signals
 data['Sell Signals'] = sell_signals
 
@@ -53,4 +54,5 @@ plt.plot(data[f'MA_{ma_2}'], label=f'MA_{ma_2}', color="yellow", linestyle="--")
 plt.scatter(data.index, data['Buy Signals'], label="Buy Signal", marker="^", color="#00ff00", lw=3)
 plt.scatter(data.index, data['Sell Signals'], label="Sell Signal", marker="v", color="#ff0000", lw=3)
 plt.legend(loc="upper left")
+
 plt.show()
--- InfinitMoneyGlitch/MACD.py
+++ InfinitMoneyGlitch/MACD.py
@@ -5,6 +5,7 @@ import numpy as np
 
 # Data setup
 symbol = 'BTC'
+
 data = yf.download(symbol, period='1y', interval='1d')
 data['EMA12'] = data['Close'].ewm(span=12, adjust=False).mean()
 data['EMA26'] = data['Close'].ewm(span=26, adjust=False).mean()
@@ -20,14 +21,17 @@ buy_price = 0
 sell_price = 0
 buy_dates = []
 sell_dates = []
+
 balance = 10000
 shares = 1
 
 
 # Algorithm
 print('Your Balance is', balance)
+
 for i in range(len(data)):
-    if data['MACD_Histogram'].iloc[i] > 0 and data['MACD_Histogram'].iloc[i] > data['MACD_Histogram'].iloc[i - 1] and not in_position:
+    if data['MACD_Histogram'].iloc[i] > 0 and data['MACD_Histogram'].iloc[i] > data['MACD_Histogram'].iloc[
+        i - 1] and not in_position:
         print(f"Buy at {data['Close'].iloc[i]}")
         buy_price = data['Close'].iloc[i]
         buy_dates.append(data.index[i])
@@ -41,14 +45,17 @@ for i in range(len(data)):
         in_position = False
         sell_executed = True
         balance = balance + sell_price * shares
+
 if in_position:
     print(f"Sell at {data['Close'].iloc[-1]}")
     sell_dates.append(data.index[-1])
+
 print('Your Balance is', balance)
 
 
 # Plotting
 plt.style.use("dark_background")
+
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 8))
 ax1.plot(data.index, data['Close'], label='Close Price', color='white')
 ax1.plot(data.index, data['EMA12'], label='12-day EMA', color='orange', linestyle="--")
@@ -57,6 +64,7 @@ ax1.scatter(buy_dates, data.loc[buy_dates, 'Close'], marker='o', color='green', 
 ax1.scatter(sell_dates, data.loc[sell_dates, 'Close'], marker='o', color='red', label='Sell')
 ax1.set_ylabel('Price')
 ax1.legend()
+
 ax2.plot(data.index, data['MACD'], label='MACD', color='cyan')
 ax2.plot(data.index, data['Signal_Line'], label='Signal Line', color='magenta')
 ax2.bar(data.index, data['MACD_Histogram'], color=np.where(data['MACD_Histogram'] > 0, 'g', 'r'))
@@ -64,4 +72,5 @@ ax2.axhline(0, linestyle='--', color='gray')
 ax2.set_xlabel('Date')
 ax2.set_ylabel('MACD')
 ax2.legend()
+
 plt.show()
